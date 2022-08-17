ce60d9d0ceab80712f2f2b8cb42542a057519689
DPB <baobo1506@gmail.com>
2022-08-18 00:45:05 +0700

    nop bai tap ss15

--- module_1/ss15_ham_function_2/bai_tap/bt4/ss15_bt4.js
+++ module_1/ss15_ham_function_2/bai_tap/bt4/ss15_bt4.js
@@ -84,13 +84,110 @@ function caculatePerimeterCircle(radiusCircle) {
 
 //Viết hàm tính giai thừa của một số nguyên bất kỳ được đưa vào. Gọi hàm vừa xây dựng được.
 function bai3() {
-
+    let enterNumber = +prompt('Nhập vào 1 số: ');
+    document.write('Giai thừa của ' + enterNumber + ' là ' + caculateFactorial(enterNumber));
+    inputBtnReloadEx();
 }
 
 function caculateFactorial(number) {
     if (number == 2) {
         return 2;
     } else {
-
+        return number * caculateFactorial(number - 1);
     }
+}
+
+function bai4() { //Viết hàm kiểm tra xem ký tự nhập vào có phải là ký tự số không. Nếu là ký tự số hàm trả về true, ngược lại trả về false.
+    let enterNumber = prompt('Nhập vào 1 số: ');
+    if (checkIsNumber(enterNumber)) {
+        document.write(enterNumber + ' là 1 số');
+    } else {
+        document.write(enterNumber + ' không phải là 1 số');
+    }
+    inputBtnReloadEx();
+}
+
+function checkIsNumber(number) {
+    if (!isNaN(+number)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function bai5() { //Viết hàm nhận vào 3 số nguyên bất kỳ, trả về số nguyên có giá trị nhỏ nhất.
+    let number1 = +prompt('Nhập vào số thứ 1: ');
+    let number2 = +prompt('Nhập vào số thứ 2: ');
+    let number3 = +prompt('Nhập vào số thứ 3: ');
+    document.write('Số nhỏ nhất là: ' + checkIsMin(number1, number2, number3));
+    inputBtnReloadEx();
+}
+
+function checkIsMin(number1, number2, number3) {
+    let numberisMin = Math.min(number1, number2, number3);
+    return numberisMin;
+}
+
+function bai6() {//Viết hàm nhận vào một số, kiểm tra xem nếu số đưa vào có phải là nguyên dương không. Nếu là nguyên dương trả về true, ngược lại trả về false.
+    let number = +prompt('Nhập vào một số:');
+    document.write(checkHighThan0(number));
+    inputBtnReloadEx();
+}
+
+function checkHighThan0(number) {
+    return number > 0 ? true : false;
+}
+
+function bai7() {//Viết hàm truyền vào 2 số nguyên bất kì, thực hiện đổi chỗ hai số nguyên đó.
+    let number1 = +prompt('Nhập vào số thứ nhất: ');
+    let number2 = +prompt('Nhập vào số thứ hai:');
+    document.write('Số thứ nhất là: ' + number1 + ' và Số thứ hai là: ' + number2);
+    // b = a + (a = b) - b;
+    // console.log(a + ' ' + b);
+    swapTwoNumber(number1, number2);
+    inputBtnReloadEx();
+}
+
+function swapTwoNumber(number1, number2) {
+    // let saveNumber = number1;
+    number2 = number1 + (number1 = number2) - number2;
+    // number2 = saveNumber;
+    document.write('Sau khi hoán đổi : Số thứ nhất là: ' + number1 + ' và Số thứ 2 là: ' + number2);
+}
+
+function bai8() {//Viết hàm truyền vào mảng số nguyên bất kỳ, sau đó đảo ngược mảng số nguyên đó.
+    reverseArray(createDimensionalArray());
+    inputBtnReloadEx();
+}
+
+function createDimensionalArray() {
+    let sizeArray = +prompt('Nhập vào độ dài của mảng: ');
+    let arrUnChecked = [];
+    for (let i = 0; i < sizeArray; i++) {
+        arrUnChecked[i] = prompt('Nhập và phần tử thứ ' + (i + 1) + ' của mảng');
+    }
+    // document.write('Các phần tử trong mảng vừa nhập là: ' + arrUnChecked.join(', ') + '<br>');
+    return arrUnChecked;
+}
+
+function reverseArray(arr) {
+    arr = arr.reverse();
+    document.write(arr.join(','));
+}
+
+function bai9() {//Viết hàm truyền vào một mảng ký tự, và một ký tự bất kỳ, kiểm tra xem ký tự bất kỳ đó có nằm trong mảng không. Nếu có trả về số lần xuất hiện của ký tự đó, nếu không trả về -1. 
+    let arr = createDimensionalArray();
+    let randomString = prompt('Nhập vào 1 ký tự bất kì: ');
+    document.write(checkHasInArray(arr, randomString));
+    inputBtnReloadEx();
+}
+
+function checkHasInArray(arr, randomString) {
+    let countHasInArrayTimes = 0;
+    for (const value of arr) {
+        if (value === randomString) {
+            countHasInArrayTimes++;
+        }
+    }
+    return countHasInArrayTimes > 0 ? countHasInArrayTimes : -1;
 }
