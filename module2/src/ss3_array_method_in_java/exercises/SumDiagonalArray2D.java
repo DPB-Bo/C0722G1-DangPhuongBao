f93365a50fb47db52f1b0c9ce3640f16c59c0271
DPB <baobo1506@gmail.com>
2022-09-08 13:33:31 +0700

    push_exercises_ss3

---
+++ module2/src/ss3_array_method_in_java/exercises/SumDiagonalArray2D.java
@@ -0,0 +1,64 @@
+package ss3_array_method_in_java.exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumDiagonalArray2D {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static Integer[] createArray(Integer sizeArray) {
+        Integer[] arr = new Integer[sizeArray];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("Nhập vào phần tử thứ %d: ", (i + 1));
+            arr[i] = Integer.parseInt(scanner.nextLine());
+        }
+        return arr;
+    }
+
+    public static Integer sizeArray(int amountDArrays) {
+        int sizeArray;
+        do {
+            System.out.printf("Nhập vào độ dài của mảng %d chiều: ", amountDArrays);
+            sizeArray = Integer.parseInt(scanner.nextLine());
+            if (sizeArray < 1) System.out.println("Vui lòng nhập độ dài hợp lệ!");
+        } while (sizeArray < 1);
+        return sizeArray;
+    }
+
+    public static void displayTwoDimensionalArray(Integer[][] arr) {
+        System.out.print("Mảng 2 chiều vừa nhập là: ");
+        for (Integer[] item : arr) {
+            System.out.print(Arrays.toString(item));
+        }
+        System.out.print("\n");
+    }
+
+    public static Integer[][] createTwoDimensionalArray() {
+        Integer amountChildArrays = sizeArray(2);
+        Integer[][] arrTwoDimensional = new Integer[amountChildArrays][];
+        for (int i = 0; i < arrTwoDimensional.length; i++) {
+            System.out.println("Nhập vào mảng thứ " + (i + 1));
+            arrTwoDimensional[i] = createArray(sizeArray(1));
+        }
+        return arrTwoDimensional;
+    }
+
+    public static void main(String[] args) {
+        Integer[][] arr2D = createTwoDimensionalArray();
+        displayTwoDimensionalArray(arr2D);
+        Integer sumDiagonalLeft = 0;
+        Integer sumDiagonalRight = 0;
+
+        for (int i = 0; i < arr2D.length; i++) {
+            if (i < arr2D[i].length) sumDiagonalLeft += arr2D[i][i];
+        }
+        for (int i = 0; i < arr2D.length; i++) {
+            if ((arr2D[i].length - 1 - i) >= 0)
+                sumDiagonalRight += arr2D[i][arr2D[i].length - 1 - i];
+        }
+        Integer sum2Diagonal = sumDiagonalLeft + sumDiagonalRight;
+        System.out.printf("Tổng đường chéo phải là %d: " +
+                "\nTổng đường chéo trái là %d: " +
+                "\nTổng 2 đường chéo là: %d", sumDiagonalLeft, sumDiagonalRight, sum2Diagonal);
+    }
+}
