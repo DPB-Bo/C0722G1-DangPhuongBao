db4a305127bce269346003d6dda63b92181a3bf1
DPB <baobo1506@gmail.com>
2022-10-17 22:54:28 +0700

    update_100%_task_18/10_cs_db

--- module3/src/cs_database/task.sql
+++ module3/src/cs_database/task.sql
@@ -19,7 +19,6 @@ WHERE ma_loai_khach = 1
 GROUP BY ma_khach_hang
 ORDER BY so_lan_dat_phong ASC;
 
--- , loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, (dich_vu.chi_phi_thue + hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia) tong_tien
 --- Task 5 ---
 SELECT khach_hang.ma_khach_hang,
 khach_hang.ho_ten,
@@ -28,11 +27,17 @@ hop_dong.ma_hop_dong,
 dich_vu.ten_dich_vu,
 hop_dong.ngay_lam_hop_dong, 
 hop_dong.ngay_ket_thuc,
+dich_vu.chi_phi_thue,
+hop_dong_chi_tiet.so_luong,
+dich_vu_di_kem.gia,
 (ifnull(dich_vu.chi_phi_thue,0)  + ifnull(hop_dong_chi_tiet.so_luong,0)  * ifnull(dich_vu_di_kem.gia,0)  ) AS tong_tien
 FROM khach_hang
-LEFT JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 INNER JOIN loai_khach ON loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
-LEFT JOIN dich_vu ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+LEFT JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 LEFT JOIN hop_dong_chi_tiet ON hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
-LEFT JOIN dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem =dich_vu_di_kem.ma_dich_vu_di_kem
+LEFT JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+LEFT JOIN dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+GROUP BY ma_khach_hang,ma_hop_dong
 ORDER BY ma_khach_hang ASC , ma_hop_dong DESC;
+
+--- Task 6 ---
