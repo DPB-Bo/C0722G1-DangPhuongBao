ecb54cb1299f22eeb8dc6bac19b9b2d503d2898d
DPB <baobo1506@gmail.com>
2022-12-23 13:55:01 +0700

    update_cs_furama

--- module_5/cs-furama/src/app/component/facility/facility-list/facility-list.component.ts
+++ module_5/cs-furama/src/app/component/facility/facility-list/facility-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Facility} from '../../../model/facility/Facility';
+import {FacilityType} from '../../../model/facility/FacilityType';
+import {RentType} from '../../../model/facility/RentType';
 
 @Component({
   selector: 'app-facility-list',
@@ -6,8 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./facility-list.component.css']
 })
 export class FacilityListComponent implements OnInit {
+  facility: Facility = {};
+  villa: FacilityType = {id: 1, name: 'Villa'};
+  house: FacilityType = {id: 2, name: 'House'};
+  room: FacilityType = {id: 3, name: 'Room'};
+  day: RentType = {id: 1, name: 'Day'};
+  month: RentType = {id: 2, name: 'Month'};
+  year: RentType = {id: 3, name: 'Year'};
+  Hour: RentType = {id: 4, name: 'Hour'};
+  facilityList: Facility[] = [
+    {
+      id: 1,
+      name: 'Villa1',
+      area: 1,
+      cost: 1,
+      maxPeople: 1,
+      facilityType: this.villa,
+      rentType: this.day,
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'có mèo',
+      poolArea: 15,
+      numberOfFloors: 3
+    },
+    {
+      id: 2,
+      name: 'House2',
+      area: 2,
+      cost: 2,
+      maxPeople: 2,
+      facilityType: this.house,
+      rentType: this.year,
+      standardRoom: 'vip',
+      descriptionOtherConvenience: 'có mèo',
+      numberOfFloors: 3
+    },
+    {
+      id: 3,
+      name: 'Room3',
+      area: 3,
+      cost: 3,
+      maxPeople: 3,
+      facilityType: this.room,
+      rentType: this.month,
+      facilityFree: 'Có khăn tắm'
+    },
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
